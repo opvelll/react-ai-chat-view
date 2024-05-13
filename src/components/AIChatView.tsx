852f384aaf6d575d353035757d8f3e8e78e6b9f2
opvel <opvel.hero@gmail.com>
2024-05-14 02:12:45 +0900

    chore: Update package.json and vite.config.ts for CSS injection plugin support

--- src/components/AIChatView.tsx
+++ src/components/AIChatView.tsx
@@ -1,22 +1,22 @@
-"use client"
 import HeaderMenu from "./HeaderMenu";
 import { useChat, ChatProp } from "./useChat";
 import ChatView from "./ChatView";
+import React from 'react';
 
-export default function AIChatView(chatProp: ChatProp) {
+const AIChatView: React.FC<ChatProp> = (chatProp: ChatProp) => {
     const {
         resetChat,
         ...props
     } = useChat(chatProp);
 
-
     return (
-        <><div className="w-full">
+        <div className="w-full">
             <HeaderMenu resetChat={resetChat} />
             <div className="flex flex-row w-full">
                 <ChatView {...props} />
             </div>
         </div>
-        </>
     )
 }
+
+export default AIChatView;
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ exclude: ["src/main.tsx", "src/App.tsx"] })],
+  plugins: [
+    react(),
+    dts({ exclude: ["./src/main.tsx", "./src/App.tsx"] }),
+    cssInjectedByJsPlugin(),
+  ],
   build: {
     lib: {
-      entry: "src/index.ts",
+      entry: "./src/index.ts",
       name: "ReactAIChatView",
-      formats: ["es"],
+      formats: ["es", "umd"],
       fileName: (format) => `react-ai-chat-view.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
     },
   },
 });
