9f1ca11101abcc9e8b5364c978860b81c5b76cb3
opvel <opvel.hero@gmail.com>
2024-05-25 23:45:08 +0900

    chore: Update HeaderMenu

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -15,41 +15,47 @@ export default function HeaderMenu({ resetChat, isOudio, modelList }: HeaderMenu
     const setModel = useChatStore((state) => state.setModel);
 
     return (
-        <header className="sticky top-0 z-50 w-full px-2 py-2 flex items-center border-b bg-white">
-            <div className="flex w-full text-lg gap-x-0.5 flex-row-reverse">
-                <button
-                    onClick={resetChat}
-                    className=" bg-white hover:bg-slate-300 font-bold py-1 px-3 rounded-full border"
-                    title="Reset Chat"
-                >
-                    <IoReload />
-                </button>
-
-                {isOudio && <button
-                    onClick={toggleAudio}
-                    className=" bg-white hover:bg-slate-300 font-bold py-1 px-3 rounded-full"
-                    title={isRunAudio ? "Mute Audio" : "Unmute Audio"}
-                >
-                    {isRunAudio ? <HiSpeakerWave /> : <HiSpeakerXMark />}
-                </button>}
-                <div className="flex-1"></div>
-                <div className="inline-block text-sm ml-1">
-                    <select
-                        id="model-select"
-                        className="form-select inline-block mt-1 border border-gray-300 ml-1"
-                        value={modelName}
-                        onChange={(event) => setModel(event.target.value)}
-                        title="Select Model"
+        <header className="sticky top-0 z-50 w-full bg-white">
+            <div className=" w-full p-2 flex flex-col border-b">
+                <div className="flex w-full text-lg gap-x-0.5 flex-row-reverse">
+                    <button
+                        onClick={resetChat}
+                        className=" bg-white hover:bg-slate-300 font-bold py-1 px-3 rounded-full border"
+                        title="Reset Chat"
                     >
-                        {(modelList as unknown as string[]).map((value, i) => (
-                            <option key={i} value={value}>
-                                {value}
-                            </option>
-                        ))}
-                    </select>
+                        <IoReload />
+                    </button>
+
+                    {isOudio && <button
+                        onClick={toggleAudio}
+                        className=" bg-white hover:bg-slate-300 font-bold py-1 px-3 rounded-full"
+                        title={isRunAudio ? "Mute Audio" : "Unmute Audio"}
+                    >
+                        {isRunAudio ? <HiSpeakerWave /> : <HiSpeakerXMark />}
+                    </button>}
+                    <div className="flex-1"></div>
+                    <div className="inline-block text-sm ml-1">
+                        <select
+                            id="model-select"
+                            className="form-select inline-block mt-1 border border-gray-300 ml-1"
+                            value={modelName}
+                            onChange={(event) => setModel(event.target.value)}
+                            title="Select Model"
+                        >
+                            {(modelList as unknown as string[]).map((value, i) => (
+                                <option key={i} value={value}>
+                                    {value}
+                                </option>
+                            ))}
+                        </select>
+
+                    </div>
 
                 </div>
 
+            </div>
+            <div className="w-full bg-gray-200 h-0.5 dark:bg-gray-700">
+                <div className="bg-blue-400 h-0.5 dark:bg-blue-500" style={{ width: "40%" }}></div>
             </div>
         </header>
     )
