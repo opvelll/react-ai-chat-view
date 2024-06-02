ed20dd2ca9f0318488dd009efe867bf390f362e1
opvel <opvel.hero@gmail.com>
2024-06-02 12:53:05 +0900

    feat: Optimize Vite configuration and plugins

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-//import wasm from "vite-plugin-wasm";
-//import topLevelAwait from "vite-plugin-top-level-await";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     dts({ exclude: ["./src/main.tsx", "./src/App.tsx"] }),
     cssInjectedByJsPlugin(),
-    // wasm(),
-    ///    topLevelAwait(),
   ],
   build: {
     lib: {
